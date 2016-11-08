f48ebf40b4b6acb41b786863a283e25673031c8d
tpertinac <topertina@gmail.com>
2016-11-08 16:40:30 +0100

    Dodan bottom nav bar Dodana home ikona u bottom nav bar(bez funkcionalnosti)

--- FOIRun/app/src/main/java/hr/foi/air/foirun/HomeActivity.java
+++ FOIRun/app/src/main/java/hr/foi/air/foirun/HomeActivity.java
@@ -1,13 +1,21 @@
 package hr.foi.air.foirun;
 
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import butterknife.ButterKnife;
+
+
 public class HomeActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(this);
     }
+
 }
