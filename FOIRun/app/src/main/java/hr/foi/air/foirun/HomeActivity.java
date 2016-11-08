4253a424287074b2d2b9e6c5f8678857947b2d93
tpertinac <topertina@gmail.com>
2016-11-08 21:18:37 +0100

    Dodana funkcionalnost home itemu

--- FOIRun/app/src/main/java/hr/foi/air/foirun/HomeActivity.java
+++ FOIRun/app/src/main/java/hr/foi/air/foirun/HomeActivity.java
@@ -1,11 +1,17 @@
 package hr.foi.air.foirun;
 
+
+import android.content.Intent;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuView;
+import android.view.View;
 
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
-
+import butterknife.OnClick;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -16,6 +22,14 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
+
+        }
+
+    @OnClick(R.id.homeItem)
+    public void homeClick(View view){
+        Intent intent = new Intent(this, HomeActivity.class);
+        startActivity(intent);
     }
 
-}
+    }
+
